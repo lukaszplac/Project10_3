f01bfcc83e109ed4a492cd70d22fa6799577bb12
lukaszplac123 <lukasz.plac@gmail.com>
2017-08-25 00:34:20 +0200

    Additional features for ambitious programmers

--- js/script.js
+++ js/script.js
@@ -1,15 +1,75 @@
 $(function() {
 	var carouselList = $("#carousel ul");
-	setInterval(changeSlides, 5000);
+	var pictureCount = carouselList.find("li").length;
+	var indicators = $(".indicators-container").find('div.picture-indicator');
+	var indicatorsContainer = $(".indicators-container");
+	var buttonPrevious = $(".navigation").find("#previous");
+	var buttonNext = $(".navigation").find("#next");
+	var currentPicture = -1;
+	var direction = -1;
 
-	function moveFirstSlide() {
+	doCount();
+	updateIndicators();
+
+
+	buttonNext.click(function(){
+		direction = -1;
+		changeSlides(direction);
+
+	});
+	buttonPrevious.click(function(){
+		direction = 1;
+		changeSlides(direction);
+	});
+	indicatorsContainer.click(function(e){
+		var clickedIndicator = e.target;
+		//tutaj skonczylem
+	});
+
+	function doCount() {
+		var intervalCounter  = setInterval(function(){ //dlaczego musze w ten sposob zmieniac ta funkcje a nie moge po prostu setInterval(changSlides(1), 500)
+			changeSlides(direction);
+		}, 5000);
+	}
+
+	function moveLeftSide() {
 		var firstItem = carouselList.find("li:first");
 		var lastItem = carouselList.find("li:last");
 		lastItem.after(firstItem);
 		carouselList.css({marginLeft:0});
 	}
 
-	function changeSlides() {
-		carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide);
+	function moveRightSide() {
+		var firstItem = carouselList.find("li:first");
+		var lastItem = carouselList.find("li:last");
+		firstItem.before(lastItem);
+		carouselList.css({marginLeft:0});
+	}
+
+	function changeSlides(direction) {
+		if (direction < 0) {
+			carouselList.animate({'marginLeft': -400 }, 500, moveLeftSide);
+			}
+		else {
+			carouselList.animate({'marginLeft': 400 }, 500, moveRightSide);
+		}
+		updateIndicators();
+	}
+
+	function updateIndicators(){
+		clearIndicators();
+		currentPicture -= direction;
+		if (currentPicture > pictureCount - 1) currentPicture = 0;
+		if (currentPicture < 0) currentPicture = pictureCount - 1;
+		markNextIndicator(currentPicture);
+	}
+
+	function clearIndicators() {
+		indicators.css('background-color', 'initial')
+	}
+
+	function markNextIndicator(currentPicture) {
+		var indicator = indicators.eq(currentPicture);
+		indicator.css('background-color','red');
 	}
 })
