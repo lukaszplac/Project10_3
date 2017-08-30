00021696f453bd4c5bdfbf31592dae46bcafc56c
lukaszplac123 <lukasz.plac@gmail.com>
2017-08-30 21:47:17 +0200

    Released after mentor comments. Now moving right worsk perfectly

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 $(function() {
+	var carousel = $('#carousel');
 	var carouselList = $("#carousel ul");
 	var pictureCount = carouselList.find("li").length;
 	var indicators = $(".indicators-container").find('div.picture-indicator');
@@ -8,6 +9,17 @@ $(function() {
 	var jumpOffset = 0;
 	var currentPicture = -1;
 	var direction = -1;
+	var intervalCounter;
+ 
+ 	carousel.hover(
+ 		function() {
+ 			clearInterval(intervalCounter);
+ 		},
+ 		function() {
+ 			doCount();
+ 		}
+ 	);
+
 
 	doCount();
 	updateIndicators();
@@ -33,7 +45,7 @@ $(function() {
 	});
 
 	function doCount() {
-		var intervalCounter  = setInterval(function(){ //1.dlaczego musze w ten sposob zmieniac ta funkcje a nie moge po prostu setInterval(changSlides(1), 500)
+		intervalCounter  = setInterval(function(){ 
 			changeSlides(direction);
 		}, 5000);
 	}
@@ -49,15 +61,15 @@ $(function() {
 		var firstItem = carouselList.find("li:first");
 		var lastItem = carouselList.find("li:last");
 		firstItem.before(lastItem);
-		carouselList.css({marginLeft:0});
+		carouselList.css({marginLeft:-400});
 	}
 
-	//2.zmiana w prawo wyglada brzydko... czy jest to problem z float czy z czyms innym ?
 	function changeSlides(direction) {
 		if (direction < 0) {
 			carouselList.animate({'marginLeft': -400 }, 500, moveLeftSide);
 			} else {
-			carouselList.animate({'marginLeft': 400 }, 500, moveRightSide);
+			moveRightSide();
+			carouselList.animate({'marginLeft': 0 }, 500);
 		}
 		updateIndicators();
 	}
@@ -70,7 +82,8 @@ $(function() {
 			}
 		} else {
 			for (var i = 0 ; i < jump; i++){
-				carouselList.animate({'marginLeft': 400 }, 150, moveRightSide);
+				moveRightSide();
+				carouselList.animate({'marginLeft': 0 }, 150);
 			}
 		}
 		updateIndicators();
@@ -97,8 +110,3 @@ $(function() {
 		indicator.css('background-color','red');
 	}
 })
-
-/*3.skad wiadomo kiedy opakowywac otrzymanego taga za pomoca jquery, czyli kiedy ponowne uzycie $ jest konieczne
-  4.kontekst this w funkcjach, zawsze sprawia mi to klopot
-  5.chialem w skrypcie uzyc clearInterval, zeby przy przesuwaniu sie o kilka zdjec (czyli jak klikam prostokat z indykatorem), kasowac czas, zeby liczyl sie od poczatku,
-	tylko nie doszedlem do tego jak po clearInterval wznowic odliczanie ponownie*/
